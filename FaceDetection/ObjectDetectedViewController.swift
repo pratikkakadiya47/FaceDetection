d024ab0824838398e936107bc867b0aea2b9b955
Binty Desai <bdesai@synoptek.com>
2019-10-23 12:10:00 +0530

    binty desai

--- FaceDetection/ObjectDetectedViewController.swift
+++ FaceDetection/ObjectDetectedViewController.swift
@@ -12,7 +12,7 @@ class ObjectDetectedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("binty desai")
+        print("binty")
         // Do any additional setup after loading the view.
     }
     
